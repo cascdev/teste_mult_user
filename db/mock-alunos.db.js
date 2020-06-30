8746092ce496a4ade1faba6dbc50d6075ef7a9bb
cascdev <charles.ufrn@gmail.com>
2020-06-29 22:33:29 -0300

    1 aluno added.

--- db/mock-alunos.db.js
+++ db/mock-alunos.db.js
@@ -1,9 +1,10 @@
 // Nosso Banco de Dados - Mockado
 let alunosLista = [
 
-	{nome:"Charles", matricula:"201902500699"}, 
+	{nome: "Charles", matricula:"201902500699"}, 
 	{nome: "Alex", matricula: "201807854709"},
-    {nome: "Adriele", matricula: "202004567599"}
+    {nome: "Adriele", matricula: "202004567599"},
+    {nome: "Pijo", matricula: "202004866932"}
     
 ]
 
