8da864a509e8a1804eae86d5ca5abd795ca9b3ee
cascdev <cascdev@gmail.com>
2020-06-29 22:08:04 -0300

    Change name variable PORTA to PORT.

--- server.js
+++ server.js
@@ -14,6 +14,6 @@ const alunosRoutes = require('./routes/alunos.routes')
 // Registre Abaixo a rota que concentra o recurso para os alunos - CRUD
 app.use( '/alunos', alunosRoutes )
 // Abaixo está a declaração do Servidor - localhost:3000
-const PORTA = 3000
-app.listen( PORTA,  _ => console.log(`Express server rodando na porta ${PORTA}`) )
+const PORT = 3000
+app.listen( PORTA,  _ => console.log(`Express server rodando na porta ${PORT}`) )
 
